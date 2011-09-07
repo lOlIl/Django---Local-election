2524a381fb8b7796a281c8d34b3d8ec9c2553174
Peter Margetiak <peter.margetiak@gmail.com>
2011-09-07 14:04:40 +0200

    * Fix #8 Question model.

--- app/election/models.py
+++ app/election/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+class Question(models.Model):
+	"""Model representing election questions"""
+	text = models.CharField(_(u'text'), max_length=50, help_text=_(u'Text of election question.'))
+    	max = models.IntegerField(_(u'max'), default=1, help_text=_(u'Maximum count of selectable answers.'))
+    	mutiple = models.BooleanField(_(u'multiple'), default=False, help_text=_(u'Multiple answers for election question.'))
+    	candidate = models.BooleanField(_(u'candidate'), default=True, help_text=_(u'Is candidate question.'))
+
+	def __unicode__(self):
+		return "(%d) %s" % (self.pk, self.text)
+
 class Election(models.Model):
 	"""Model representing election event"""
     	name = models.CharField(_(u'title'), max_length=50, help_text=_(u'Name of the election.'))
@@ -11,6 +21,7 @@ class Election(models.Model):
     	end = models.DateTimeField(_(u'end time'), blank=True, null=True, help_text=_(u'Date-time stamp of the election end.'))
     	visibility = models.BooleanField(_(u'visible'), default=True, help_text=_(u'Boolean control of election visibility.'))
     	image = models.FileField(_(u'photo'), upload_to='election/%Y/%m/%d', blank=True)
+	question = models.ManyToManyField(Question, blank=True, null=True, verbose_name=_(u'list of questions'), help_text=_(u'Questions for election.'))
 
 	def __unicode__(self):
-		return self.name 
+		return self.name
