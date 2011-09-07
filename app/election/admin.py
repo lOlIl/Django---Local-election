96326b328d703f3192709795119b449820a24115
Peter Margetiak <peter.margetiak@gmail.com>
2011-09-07 23:12:51 +0200

    * Fix #4 Candidates administration in election, added answers into Question administration

--- app/election/admin.py
+++ app/election/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from election.models import Election, Question, Answer, Voter
+from election.models import Election, Question, Answer, Voter, Candidate
 from election.forms import ElectionAdminForm
 
 class QuestionInline(admin.TabularInline):
@@ -8,18 +8,28 @@ class QuestionInline(admin.TabularInline):
 class VoterInline(admin.TabularInline):
     	model = Voter
 
+class CandidateInline(admin.TabularInline):
+    	model = Candidate
+
 class ElectionAdmin(admin.ModelAdmin):
 	list_display = ('name', 'start', 'end', 'visibility')
 	fields = ('name', 'description', 'start', 'end', 'visibility', 'image')
 	form = ElectionAdminForm
 	inlines = [
-        	QuestionInline, VoterInline
+        	QuestionInline, VoterInline, CandidateInline
     	]
 
 admin.site.register(Election, ElectionAdmin)
 
+class AnswerInline(admin.TabularInline):
+	model = Question.answer.through
+
 class QuestionAdmin(admin.ModelAdmin):
 	list_display = ('text', 'max', 'candidate', 'mutiple')
+	exclude = ('answer', )
+	inlines = [
+        	AnswerInline
+    	]
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Answer)
