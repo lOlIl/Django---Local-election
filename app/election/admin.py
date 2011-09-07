8c45522202e9999bdaa4b7c238c0c4b8f0df5b6d
Peter Margetiak <peter.margetiak@gmail.com>
2011-09-07 14:07:38 +0200

    * Fix #2 Questions in administration

--- app/election/admin.py
+++ app/election/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-from election.models import Election
+from election.models import Election, Question
 from election.forms import ElectionAdminForm
+
+class QuestionInline(admin.TabularInline):
+	model = Election.question.through
 
 class ElectionAdmin(admin.ModelAdmin):
 	list_display = ('name', 'start', 'end', 'visibility')
 	fields = ('name', 'description', 'start', 'end', 'visibility', 'image')
 	form = ElectionAdminForm
+	inlines = [
+        	QuestionInline,
+    	]
 
 admin.site.register(Election, ElectionAdmin)
+
+class QuestionAdmin(admin.ModelAdmin):
+	list_display = ('text', 'max', 'candidate', 'mutiple')
+
+admin.site.register(Question, QuestionAdmin)
--- app/election/forms.py
+++ app/election/forms.py
@@ -1,7 +1,11 @@
 from django import forms
-from election.models import Election
+from election.models import Election, Question
 from django.utils.translation import ugettext_lazy as _
 
 class ElectionAdminForm(forms.ModelForm):
 	class Meta:
 		model = Election
+
+class QuestionAdminForm(forms.ModelForm):
+	class Meta:
+		model = Question
